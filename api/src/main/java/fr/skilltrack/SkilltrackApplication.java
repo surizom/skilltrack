0588a366f2254946258e546bad70eebcae6e6d06
Moaad Fattali <f.moaad@gmail.fr>
2020-06-21 18:52:22 +0200

    bootstrap spring API

---
+++ api/src/main/java/fr/skilltrack/SkilltrackApplication.java
@@ -0,0 +1,13 @@
+package fr.skilltrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkilltrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkilltrackApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/fr/skilltrack/SkilltrackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.skilltrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkilltrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
