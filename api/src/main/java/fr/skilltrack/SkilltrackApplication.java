1cd156b7c562adb80657125efacca967e74ac92a
Moaad Fattali <f.moaad@gmail.fr>
2020-06-23 22:59:32 +0200

    add graphQL back dependencies

--- api/src/main/java/fr/skilltrack/SkilltrackApplication.java
+++ api/src/main/java/fr/skilltrack/SkilltrackApplication.java
@@ -1,7 +1,11 @@
 package fr.skilltrack;
 
+import graphql.Scalars;
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.GraphQLSchema;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SkilltrackApplication {
@@ -10,4 +14,17 @@ public class SkilltrackApplication {
 		SpringApplication.run(SkilltrackApplication.class, args);
 	}
 
+	@Bean
+	GraphQLSchema schema() {
+		return GraphQLSchema.newSchema()
+				.query(GraphQLObjectType.newObject()
+						.name("query")
+						.field(field -> field
+								.name("test")
+								.type(Scalars.GraphQLString)
+								.dataFetcher(environment -> "response")
+						)
+						.build())
+				.build();
+	}
 }
