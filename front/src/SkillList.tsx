bd97de668dc40daa50b6a1b42e357e2a3ffa8072
MoaadF <moaadf@theodo.fr>
2020-07-02 18:58:11 +0200

    configure eslint & prettier

--- front/src/SkillList.tsx
+++ front/src/SkillList.tsx
@@ -3,21 +3,33 @@ import { useQuery } from '@apollo/react-hooks';
 import { SKILLS } from './queries';
 import type { Skill } from './generated/graphql';
 
-const SkillList : React.FunctionComponent<{}> = ({}) => {
-
-  const {loading, error, data}= useQuery<{ skills: Skill[] }>(SKILLS)
+const SkillList: React.FunctionComponent = ({}) => {
+  const { loading, error, data } = useQuery<{ skills: Skill[] }>(SKILLS);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error</p>;
 
-  if (!data || !data.skills || !data.skills.length ){
-    return <div>No skills to show</div>
+  if (!data || !data.skills || !data.skills.length) {
+    return <div>No skills to show</div>;
   }
 
-  return <div>
-    {data.skills.map(({ name, id, importance }: { name: string, id: string, importance: number }) =>
-      <div key={id}>{name}</div>)}
-  </div>
-}
+  return (
+    <div>
+      {data.skills.map(
+        ({
+          name,
+          id,
+          importance,
+        }: {
+          name: string;
+          id: string;
+          importance: number;
+        }) => (
+          <div key={id}>{name}</div>
+        ),
+      )}
+    </div>
+  );
+};
 
 export default SkillList;
