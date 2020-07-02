48a65d8c40382315d6f0ce863a4135dba2518720
MoaadF <moaadf@theodo.fr>
2020-07-02 18:39:43 +0200

    rewrite SkillList component to use automatically generated types

--- front/src/SkillList.tsx
+++ front/src/SkillList.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { SKILLS } from './queries';
+import type { Skill } from './generated/graphql';
 
-const SkillList : React.FunctionComponent = () => {
+const SkillList : React.FunctionComponent<{}> = ({}) => {
 
-  const {loading, error, data}= useQuery(SKILLS)
+  const {loading, error, data}= useQuery<{ skills: Skill[] }>(SKILLS)
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error</p>;
 
-  return data && data.skills && data.skills.map(({name, id, importance}:{name: string, id:string, importance:number})=> <div key={id}>{name}</div>)
+  if (!data || !data.skills || !data.skills.length ){
+    return <div>No skills to show</div>
+  }
+
+  return <div>
+    {data.skills.map(({ name, id, importance }: { name: string, id: string, importance: number }) =>
+      <div key={id}>{name}</div>)}
+  </div>
 }
 
 export default SkillList;
