af829ec46ec3dc69f1d5032c747b179c0c4f712e
MoaadF <moaadf@theodo.fr>
2020-07-04 19:19:31 +0200

    add custom time provider with explicitly set locale

---
+++ api/src/main/java/fr/skilltrack/time/TimeProvider.java
@@ -0,0 +1,17 @@
+package fr.skilltrack.time;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+public class TimeProvider {
+        public static final ZoneId ZONE_ID = ZoneId.of("Europe/Paris");
+
+        public static Instant now() {
+            return Instant.now();
+        }
+
+        public static LocalDate today() {
+            return LocalDate.now(ZONE_ID);
+        }
+}
