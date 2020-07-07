a344cecdd8505c5d6f52f2f23c8918ef3e39783d
MoaadF <f.moaad@gmail.com>
2020-07-07 18:57:34 +0200

    change white background

--- front/src/common/style/palette.ts
+++ front/src/common/style/palette.ts
@@ -3,5 +3,5 @@ export const secondary = '#2a9d8f';
 export const tertiary = '#e9c46a';
 export const quartenary = '#f4a261';
 export const quintary = '#e76f51';
-export const whiteBackground = '#ffffff';
+export const whiteBackground = '#f8f8fb';
 export const blackBackground = '#2e2e2e';
