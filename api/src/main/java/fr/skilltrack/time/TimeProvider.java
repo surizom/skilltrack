2d95404a9ddc3bbd50fdbe80752fa97ef5617085
MoaadF <f.moaad@gmail.com>
2020-07-14 21:06:29 +0200

    make time provider a service and add date->integer methods

--- api/src/main/java/fr/skilltrack/time/TimeProvider.java
+++ api/src/main/java/fr/skilltrack/time/TimeProvider.java
@@ -1,17 +1,38 @@
 package fr.skilltrack.time;
 
+import org.springframework.stereotype.Service;
+
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 
+@Service
 public class TimeProvider {
+  private final LocalDate beginningDay;
+
+  public TimeProvider() {
+    this.beginningDay = LocalDate.of(2020, 6, 15);
+  }
+
   public static final ZoneId ZONE_ID = ZoneId.of("Europe/Paris");
 
-  public static Instant now() {
+  public Instant now() {
     return Instant.now();
   }
 
-  public static LocalDate today() {
+  public LocalDate today() {
     return LocalDate.now(ZONE_ID);
+  }
+
+  public LocalDate beginning() {
+    return this.beginningDay;
+  }
+
+  public Integer numberOfDaysSinceBeginning() {
+    return today().compareTo(this.beginningDay);
+  }
+
+  public Integer numberOfDaysSinceBeginning(LocalDate date) {
+    return date.compareTo(beginning());
   }
 }
